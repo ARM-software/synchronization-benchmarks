83f3f704093b18e16e3f0d7732b3fef0520d1aeb
jty2 <46021128+jty2@users.noreply.github.com>
2025-05-30 16:41:10 -0500

    benchmarks/lockhammer/include/verbose.h - verbosity definitions
    Change-Id: Id6754ea81e433b2dc38e359e16150f38bbf40099

---
+++ benchmarks/lockhammer/include/verbose.h
@@ -0,0 +1,19 @@
+
+/*
+ * SPDX-FileCopyrightText: Copyright 2024-2025 Arm Limited and/or its affiliates <open-source-office@arm.com>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef VERBOSE_H
+#define VERBOSE_H
+
+enum {
+    VERBOSE_MORE=3,
+    VERBOSE_YES=2,
+    VERBOSE_LOW=1,    // default
+    VERBOSE_NONE=0    // to-be-implemented
+};
+
+#endif
+
+/* vim: set tabstop=4 shiftwidth=4 softtabstop=4 expandtab: */
